66ca922507b315c05fd1f7a8e989999e45c62f99
Mani Shidfar <mreza.shidfar@gmail.com>
2023-02-23 21:27:34 +0100

    pokemondex proj done!

--- js/script.js
+++ js/script.js
@@ -4,123 +4,146 @@ const pokemons =  [
       id:'N°01' ,
       name: 'bulbasaur',
       type: ['plant', 'poison'],
+      bgColor:['orange','purple']
+      
     },
     {
       thumbnail: './assets/002.png',
       id:'N°02',
       name: 'charmander',
       type: ['fire'],
+      bgColor:['yellow' , 'gray']
+      
     },
     {
       thumbnail: './assets/003.png',
       id:'N°03',
       name: 'caterpie',
       type: ['bug'],
+      bgColor:['gray']
+      
     },
     {
       thumbnail: './assets/004.png',
       id:'N°04',
       name: 'kaluna',
       type: ['bug', 'poison'],
+      bgColor:['green','gray']
+      
     },
     {
       thumbnail: './assets/005.png',
       id:'N°05',
       name: 'ratata',
       type: ['plant'],
+      bgColor:['green','orange']
+    
     },
     {
       thumbnail: './assets/006.png',
       id:'N°06',
       name: 'pikachu',
       type: ['normal'],
+      bgColor:['yellow', 'pink']
     },
     {
       thumbnail: './assets/007.png',
       id:'N°07',
       name: 'vulpix',
       type: ['fire', 'plant'],
+      bgColor:['pink','purple']
     },
     {
       thumbnail: './assets/008.png',
       id:'N°08',
       name: 'jigglypuff',
       type: ['normal', 'fairy'],
+      bgColor:['orange', 'gray']
+      
     },
     {
       thumbnail: './assets/009.png',
       id:'N°09',
       name: 'ice',
       type: ['bug'],
+      bgColor:['lightgray','pink']
     },
     {
       thumbnail: './assets/010.png',
       id:'N°10',
       name: 'ground',
       type: ['electrical'],
+      bgColor:['yellow', 'purple']
     },
     {
       thumbnail: './assets/011.png',
       id:'N°11',
       name: 'ghost',
       type: ['poison'],
+      bgColor:['gray', 'pink']
     },
     {
       thumbnail: './assets/012.png',
       id:'N°12',
       name: 'deer',
       type: ['fire'],
+      bgColor:['green','yellow']
     },
     {
       thumbnail: './assets/013.png',
       id:'N°13',
       name: 'dark',
       type: ['fairy'],
+      bgColor:['gray', 'pink']
     },
     {
       thumbnail: './assets/014.png',
       id:'N°14',
       name: 'stell',
       type: ['normal', 'electrical'],
+      bgColor:['purple', 'orange']
     },
     {
       thumbnail: './assets/015.png',
       id:'N°15',
       name: 'cat',
       type: ['fire'],
+      bgColor:['yellow', 'pink']
     },
     {
       thumbnail: './assets/016.png',
       id:'N°16',
       name: 'rock',
       type: ['fairy', 'poison'],
+      bgColor:['green', 'gray']
     }
   ]
+  const colors = ['purple', 'lightgray' , 'orange' , 'lightgreen' , 'lightpink'];
   const myContainer = document.querySelector('#div-container')
    pokemons.forEach(item => {
       const img = document.createElement('img');
       const id = document.createElement('p')
       const nombre = document.createElement('h4');
-      const type = document.createElement('p')
       const card = document.createElement('div');
-      const span1 = document.createElement('span');
-      const span2 = document.createElement('span');
-      span1.className='span1';
-      span2.className = 'span2';
       const spanDiv = document.createElement('div');
       spanDiv.className = 'span-div';
 
-
+       
       img.src = item.thumbnail;
       id.innerHTML = item.id;
       nombre.innerHTML = item.name;
-      for(let i = 0; i<item.type.length; i++) {
+      for(let i = 0; i < item.type.length; i++) {
+        const span = document.createElement('span');
+         span.innerHTML = item.type[i]
+         span.style.backgroundColor = item.bgColor[i];
+         span.style.color='white'
+         spanDiv.append(span);
         
-        span1.innerHTML = item.type[0] + " ";
-        span2.innerHTML = item.type[1]
-      
+         
       }
-      spanDiv.append(span1,span2)
+     
+   
+      
       card.append(img , id, nombre,spanDiv);
       card.className='my-card';
       myContainer.appendChild(card)
@@ -128,3 +151,4 @@ const pokemons =  [
   
   
   })
+  
